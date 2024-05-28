6b712319997259664bdaf6d97b656b1f25a6ffd0
ErwinAdriel <adrielquispe87@gmail.com>
2024-05-28 20:35:05 -0300

    Inicio proyecto

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return(
+        <div className='navbarContainer'>
+            <p>Navegacion</p>
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Nosotros</li>
+                <li>Contacto</li>
+            </ul>
+        </div>
+    )
+}
